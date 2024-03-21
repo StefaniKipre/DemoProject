c4c6bd94a7900f81b9812c1c1225f98a5d163589
StefaniKipre <stefani.kiprevska@gmail.com>
2024-03-21 14:10:19 +0100

    mochawesome reporter is working and some files are deleted

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,22 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "test/cypress/e2e/**/*.*",
+    testIsolation: false,
+    chromeWebSecurity: false,
+    reporter: 'mochawesome',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      on('task', {
+        log(message) {
+          // Then to see the log messages in the terminal
+          //   cy.task("log", "my message");
+          console.log(message +'\n\n');
+          return null;
+        
+        },
+      });
     },
   },
 });
